c6a4c4765203807c9dd93d5311daf7669293dc8c
Shashwath Sunkum <shashsunkum@gmail.com>
2024-04-18 09:15:52 -0400

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.rtl.css'
+import Login from "./Components/Login.jsx";
+
+function App() {
+
+  return (
+    <><Login /></>
+  )
+}
+
+export default App
---
+++ src/Components/Login.jsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './style.css';
+
+const Login = () => {
+    return(
+        <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+            <div className="p-3 rounded w-25 border loginForm">
+                <h2>Login Page</h2>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="email"><strong>Email:</strong></label>
+                        <input type="email" id="email" name="email" autoComplete="off" placeholder="Enter Email"
+                               className="form-control rounded-0"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password"><strong>Password:</strong></label>
+                        <input type="password" id="password" name="password" placeholder="Enter Password"
+                               className="form-control rounded-0"/>
+                    </div>
+                    <button className="btn btn-success w-100 rounded-0 mb-2">Log in</button>
+                    <div className="mb-1">
+                        <input type="checkbox" name="tick" id="tick" className="me-2"/>
+                        <label htmlFor="password">Check to agree Terms & Conditions</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
